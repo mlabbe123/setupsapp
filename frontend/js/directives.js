19dce20a2afaa4e17f12975260cd42737e59c152
Mathieu Labbé <mathieu.labbe@hotmail.com>
2015-08-09 10:31:10 -0400

    Begun tooltip module

--- frontend/js/directives.js
+++ frontend/js/directives.js
@@ -13,3 +13,22 @@ setupsSharingAppDirectives.directive('customFileUpload', [function() {
         }
     }
 }]);
+
+setupsSharingAppDirectives.directive('tooltip', [function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var onMouseEnterFunc = function() {
+                var toolTipElement = '<div class="tooltip-wrapper">' + attrs.tooltip + '</div>';
+                element.append(angular.element(toolTipElement));
+            };
+
+            var onMouseLeaveFunc = function() {
+                element[0].querySelector('.tooltip-wrapper').remove();
+            };
+
+            element.bind('mouseenter', onMouseEnterFunc);
+            element.bind('mouseleave', onMouseLeaveFunc);
+        } 
+    }
+}]);
