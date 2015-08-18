6f31f418b4788b93c90d583315508b7514e0533b
Mathieu Labbé <mathieu.labbe@hotmail.com>
2015-08-18 10:55:41 -0400

    dbuser and pass are noew process env vars

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mlabbe:GlaspRob321@ds051738.mongolab.com:51738/setupmarket');
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@ds051738.mongolab.com:51738/setupmarket');
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var db = require('./config/db'),
     flash = require('connect-flash'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
-    // multer = require('multer'),
     fs = require('fs');
 
     _ = require('lodash');
@@ -21,8 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
     secret: "cookie_secret",
     name: "cookie_name",
-    //store: sessionStore, // connect-mongo session store
-    //proxy: true,
     resave: true,
     saveUninitialized: true
 }));
@@ -33,43 +30,11 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-// Multer config (file upload)
-// app.use(multer({ dest: './setups_files/',
-//     rename: function (fieldname, filename) {
-//         return filename;
-//     },
-//     changeDest: function(dest, request, response) {
-//         var stat = null;
-
-//         try {
-//             stat = fs.statSync(dest + request.body.sim);
-//         } catch (err) {
-//             fs.mkdirSync(dest + request.body.sim);
-//         }
-
-//         if (stat && !stat.isDirectory()) {
-//             throw new Error('Directory cannot be created because an inode of a different type exists at "' + dest + request.body.sim + '"');
-//         }
-
-//         return dest + request.body.sim;
-//     },
-//     onFileUploadStart: function (file) {
-//         console.log(file.originalname + ' is starting ...')
-//         // console.log('encoding', file.encoding)
-//         // console.log('mimetype', file.mimetype)
-//         // console.log('extension', file.extension)
-//     },
-//     onFileUploadComplete: function (file) {
-//         console.log(file.fieldname + ' uploaded to  ' + file.path)
-//         done=true;
-//     }
-// }));
-
 // Routes config
 require('./app/routes.js')(app, passport);
 
 // Start the server
-var server = app.listen(process.env.PORT || 5000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
     var host = server.address().address;
     var port = server.address().port;
