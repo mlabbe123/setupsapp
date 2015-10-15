2aafeba34a3fa460de69f08ba2503a33a6002fb2
Mathieu Labbé <mathieu.labbe@hotmail.com>
2015-10-15 16:36:42 -0400

    Ctrl for edit setup updated

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -304,7 +304,30 @@
             angular.element(document.getElementsByClassName('main-menu-link-setups')).addClass('current');
         })
 
-        .controller('setupEditCtrl', function($scope, $routeParams, $http, $location) {
+        .controller('setupEditCtrl', function($scope, $routeParams, $http, $location, userSession, $timeout, uploadSetupService) {
+
+            // Notifications
+            $scope.notification = {};
+            $scope.notification.status = userSession.getNotificationStatus();
+            $scope.notification.msg = userSession.getNotificationMsg();
+
+            $scope.$watch(userSession.getNotificationMsg, function() {
+                $scope.notification = {};
+                $scope.notification.msg = userSession.getNotificationMsg();
+                $scope.notification.status = userSession.getNotificationStatus();
+            });
+
+            $scope.closeNotification = function() {
+                $scope.notification = false;
+                userSession.setNotificationStatus('');
+                userSession.setNotificationMsg('');
+            };
+
+            $timeout(function() {
+                console.log('close it mothafucka')
+                $scope.closeNotification();
+            }, 5000);
+
             $scope.sim_name = $routeParams.simName;
 
             // Get sim info to populate the versions dropdown.
@@ -320,6 +343,7 @@
             $http.get('/api/get-setup/' + $routeParams.setupId).
                 success(function(data, status, headers, config) {
                     $scope.setup = data;
+                    console.log(data)
                 }).
                 error(function(data, status, headers, config) {
                     console.log(status);
@@ -327,33 +351,10 @@
 
             // Sends update info to API.
             $scope.updateSetup = function() {
-                // Find the form elements.
-                var $form = angular.element(document.getElementById('update-setup-form')),
-                    trimFieldValue = angular.element(document.getElementById('trimSelect')).val(),
-                    simVersionFieldValue = angular.element(document.getElementById('versionSelect')).val(),
-                    bestLapFieldValue = angular.element(document.getElementById('bestLapInput')).val(),
-                    commentsFieldValue = angular.element(document.getElementById('commentsTextarea')).val();
+                var $element = angular.element(event.srcElement);
 
-                // Sanitize the comments field.
-                commentsFieldValue = commentsFieldValue.replace(/<(?:.|\n)*?>/gm, '');
-
-                // Build the params object.
-                var ajaxParams = {
-                    setupId: $routeParams.setupId,
-                    trim: trimFieldValue,
-                    sim_version: simVersionFieldValue,
-                    best_time: bestLapFieldValue,
-                    comments: commentsFieldValue
-                }
-
-                // Ajax post request to the API.
-                $http.post('/api/update-setup/', ajaxParams)
-                    .success(function(data, status, headers, config) {
-                        $location.path('/setups/'+$routeParams.simName+'/'+$routeParams.setupId);
-                    })
-                    .error(function(data, status, headers, config) {
-                        $scope.errorMsg = 'There has been an error for your request. Please try again or contact admins.';
-                    });
+                $element.prop('disabled', 'disabled').addClass('is-loading icon-loading');
+                uploadSetupService.update($scope.setup);
             }
 
             angular.element(document.getElementsByClassName('main-menu-link')).removeClass('current');
@@ -377,6 +378,10 @@
                 $scope.notification = false;
                 userSession.setNotificationStatus('');
                 userSession.setNotificationMsg('');
+
+                // $timeout(function() {
+                //     $scope.closeNotification();
+                // }, 5000);
             };
 
             $timeout(function() {
@@ -394,7 +399,6 @@
             }
 
             $scope.submitSetup = function(event) {
-                console.log(event)
                 var $element = angular.element(event.srcElement);
 
                 $element.prop('disabled', 'disabled').addClass('is-loading icon-loading');
